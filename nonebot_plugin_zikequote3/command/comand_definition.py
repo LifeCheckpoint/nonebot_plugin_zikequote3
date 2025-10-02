993730237cc6a5c5785764a72d056c5847901b93
lifecheckpoint <lifechpt@qq.com>
2025-10-02 12:19:22 +0800

    语录设置相关命令

--- nonebot_plugin_zikequote3/command/comand_definition.py
+++ nonebot_plugin_zikequote3/command/comand_definition.py
@@ -96,3 +96,36 @@ matcher_quote_search = on_command(
     aliases=set(_quote_search_cmds[1:]),
     priority=10, block=True, permission=permission_check(PMS.GET_QUOTE)
 )
+
+
+# settings
+_get_quote_setting_cmds = (
+    "当前语录设置", "get_quote_setting", "get_quote_config", "查看语录设置", "查看语录配置", 
+    "语录配置查看", "语录设置查看", "查看当前语录设置", "查看当前语录配置"
+)
+matcher_get_quote_setting = on_command(
+    _get_quote_setting_cmds[0],
+    aliases=set(_get_quote_setting_cmds[1:]),
+    priority=10, block=True, permission=permission_check(PMS.OTHERS)
+)
+
+_modify_quote_setting_cmds = (
+    "修改语录设置", "修改语录配置", "设置语录设置", "设置语录配置", 
+    "更改语录设置", "更改语录配置", "更新语录设置", "更新语录配置",
+    "set_quote_setting", "set_quote_config"
+)
+matcher_modify_quote_setting = on_command(
+    _modify_quote_setting_cmds[0],
+    aliases=set(_modify_quote_setting_cmds[1:]),
+    priority=10, block=True, permission=permission_check(PMS.OTHERS)
+)
+
+_reset_quote_setting_cmds = (
+    "重置语录设置", "重置语录配置", "恢复语录设置", "恢复语录配置",
+    "reset_quote_setting", "reset_quote_config"
+)
+matcher_reset_quote_setting = on_command(
+    _reset_quote_setting_cmds[0],
+    aliases=set(_reset_quote_setting_cmds[1:]),
+    priority=10, block=True, permission=permission_check(PMS.OTHERS)
+)
