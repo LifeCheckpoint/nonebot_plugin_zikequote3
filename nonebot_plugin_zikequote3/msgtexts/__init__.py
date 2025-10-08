bd7e0587360ba8d9045e5683eba900adb27ecf48
lifecheckpoint <lifechpt@qq.com>
2025-10-08 18:23:42 +0800

    修复部分模板路径问题

--- nonebot_plugin_zikequote3/msgtexts/__init__.py
+++ nonebot_plugin_zikequote3/msgtexts/__init__.py
@@ -6,8 +6,11 @@ from jinja2 import Environment, FileSystemLoader, select_autoescape
 
 # 初始化Jinja2环境
 from ..imports import module_msgtexts_root
+
+all_template_dir = [str(p.absolute()) for p in module_msgtexts_root.glob('*/')]
+
 env = Environment(
-    loader=FileSystemLoader(str(module_msgtexts_root.absolute())),
+    loader=FileSystemLoader(all_template_dir),
     autoescape=select_autoescape(['html', 'xml']),
     trim_blocks=True,
     lstrip_blocks=True
