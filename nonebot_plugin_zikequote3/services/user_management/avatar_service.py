c619ffcd01d2bd8512493fc919c357500f2c966c
lifecheckpoint <lifechpt@qq.com>
2025-10-08 16:57:37 +0800

    修复头像超时类型错误

--- nonebot_plugin_zikequote3/services/user_management/avatar_service.py
+++ nonebot_plugin_zikequote3/services/user_management/avatar_service.py
@@ -1,12 +1,13 @@
 from ...imports import *
 import aiohttp
+from aiohttp import ClientTimeout
 
 async def get_user_avatar(qq: str) -> bytes:
     """
     获取用户头像
     """
     url = f"https://q.qlogo.cn/headimg_dl?dst_uin={qq}&spec=640&img_type=jpg"
-    async with aiohttp.ClientSession(timeout=10) as session:
+    async with aiohttp.ClientSession(timeout=ClientTimeout(total=10)) as session:
         async with session.get(url) as resp:
             resp.raise_for_status()
             return await resp.read()
