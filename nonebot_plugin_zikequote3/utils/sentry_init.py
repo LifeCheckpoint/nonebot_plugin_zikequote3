02f3a5574fe576eabbeeed3f5d828ffae9efde96
lifecheckpoint <lifechpt@qq.com>
2025-10-08 17:11:12 +0800

    修复sentry启动错误

--- nonebot_plugin_zikequote3/utils/sentry_init.py
+++ nonebot_plugin_zikequote3/utils/sentry_init.py
@@ -2,22 +2,30 @@ def init_sentry():
     from ..imports import Path, default_cfg, _plugin_root, logger
     import sentry_sdk
 
-    dsn_path = Path(default_cfg.sentry.dsn_path) if default_cfg.sentry.dsn_path else None
+    dsn_path_to_parse = Path(default_cfg.sentry.dsn_path) if default_cfg.sentry.dsn_path else None
+    if dsn_path_to_parse and not dsn_path_to_parse.is_absolute():
+        dsn_path = _plugin_root / default_cfg.sentry.dsn_path
+    elif dsn_path_to_parse is not None:
+        dsn_path = dsn_path_to_parse
+    else:
+        dsn_path = None
 
-    if dsn_path is None or not dsn_path.is_file():
-        logger.info("Sentry DSN: (not set for empty path)")
+    if dsn_path is None:
+        logger.warning("Sentry DSN: (not set for empty path)")
+        return
+    
+    if not dsn_path.is_file():
+        logger.error(f"Sentry DSN: (not set, file {dsn_path} does not exist)")
         return
     
     try:
-        dsn_str = (
-            dsn_path if dsn_path.is_absolute() else (_plugin_root / dsn_path)
-        ).read_text(encoding="utf-8").strip()
+        dsn_str = dsn_path.read_text(encoding="utf-8").strip()
     except Exception as e:
         logger.error(f"Failed to read Sentry DSN from {dsn_path}: {e}")
         return
     
     if dsn_str == "":
-        logger.info("Sentry DSN: (not set for empty content)")
+        logger.warning("Sentry DSN: (not set for empty content)")
         return
 
     logger.info(f"Sentry DSN: {dsn_str[:8] + '...' }")
