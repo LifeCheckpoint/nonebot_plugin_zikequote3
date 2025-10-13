dce6766bf92b0dd97298c3a1fc58abc8ed387f71
lifecheckpoint <lifechpt@qq.com>
2025-10-13 17:14:25 +0800

    fix a typo

--- nonebot_plugin_zikequote3/command/parse_helper/__init__.py
+++ nonebot_plugin_zikequote3/command/parse_helper/__init__.py
@@ -1,3 +1,3 @@
-from aliases_cmd_definition import *
-from paramtype_range import PARAMTYPE_RANGE
+from .aliases_cmd_definition import *
+from .paramtype_range import PARAMTYPE_RANGE
 from ...utils.click_cmd_parser import parse_command
